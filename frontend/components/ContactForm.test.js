c6cf383ea3c26a782d6b1fde567f5aef218fb475
Branden Jones <branden.wayne.jones@gmail.com>
2022-10-07 22:01:28 -0400

    tests for render and header are passing

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -4,12 +4,15 @@ import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
-test('renders without errors', () => {
 
+test('renders without errors', () => {
+    render(<ContactForm />)
 });
 
 test('renders the contact form header', () => {
-
+    render(<ContactForm />)
+    const header = screen.getByText(/contact form/i);
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
